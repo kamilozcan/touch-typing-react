2e47dea622b756df364848fb216b9842842ce703
Kamil Ozcan <kamilozcann@gmail.com>
2023-10-18 14:52:07 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { generate as randomWords } from "random-words";
+
+import "./App.css";
+
+const NUMB_OF_WORDS = 200;
+const SECONDS = 60;
 
 function App() {
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    setWords(generateWords());
+  }, []);
+
+  function generateWords() {
+    return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="control is-expanded section">
+        <input type="text" className="input"/>
+
+      </div>
+      <div className="section">
+        <div className="card">
+          <div className="card-content">
+            <div className="content">
+              {words.map((word, i) => (
+                <>
+                <span>
+                  {word}
+                </span>
+                <span> </span>
+                </>
+                
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
