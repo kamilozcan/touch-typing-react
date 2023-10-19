70e7bc9f0b196cac778c56393225e6bd88ebdb30
Kamil Ozcan <kamilozcann@gmail.com>
2023-10-19 07:52:24 -0400

    Added WPM and Accurancy section

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { generate as randomWords } from "random-words";
 import "./App.css";
 
 const NUMB_OF_WORDS = 200;
-const SECONDS = 60;
+const SECONDS = 10;
 
 function App() {
   const [words, setWords] = useState([]);
   const [countDown, setCountDown] = useState(SECONDS);
-  const [currentInput, setCurrentInput] = useState("")
-  const [currentWordIndex, setCurrentWordIndex] = useState(0)
+  const [currentInput, setCurrentInput] = useState("");
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [correct, setCorrect] = useState(0)
+  const [incorrect, setInCorrect] = useState(0)
 
   useEffect(() => {
     setWords(generateWords());
@@ -32,19 +34,23 @@ function App() {
     }, 1000);
   }
 
-  function handleKeyDown({keyCode}) {
-// space bar
-    if(keyCode === 32) {
-      checkMatch()
-      setCurrentInput("") //When you click space input getting clean
-      setCurrentWordIndex(currentWordIndex + 1)
+  function handleKeyDown({ keyCode }) {
+    // space bar
+    if (keyCode === 32) {
+      checkMatch();
+      setCurrentInput(""); //When you click space input getting clean
+      setCurrentWordIndex(currentWordIndex + 1);
     }
   }
 
   function checkMatch() {
-    const wordToCompare = words[currentWordIndex]
-    const doesItMatch = wordToCompare === currentInput.trim()
-    console.log({doesItMatch})
+    const wordToCompare = words[currentWordIndex];
+    const doesItMatch = wordToCompare === currentInput.trim();
+    if(doesItMatch) {
+      setCorrect(correct + 1)
+    } else {
+      setInCorrect(incorrect = 1)
+    }
   }
 
   return (
@@ -55,7 +61,13 @@ function App() {
         </div>
       </div>
       <div className="control is-expanded section">
-        <input type="text" className="input" onKeyDown={handleKeyDown} value={currentInput} onChange={(e) => setCurrentInput(e.target.value)} />
+        <input
+          type="text"
+          className="input"
+          onKeyDown={handleKeyDown}
+          value={currentInput}
+          onChange={(e) => setCurrentInput(e.target.value)}
+        />
       </div>
       <div className="section">
         <button className="button is-info is-fullwidth" onClick={start}>
@@ -77,6 +89,19 @@ function App() {
                 </span>
               ))}
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="section">
+        <div className="columns">
+          <div className="column has-text-centered">
+            <p className="is-size-5">Words per minute:</p>
+            <p className="has-text-primary is-size-1">{correct}</p>
+          </div>
+          <div className="column has-text-centered">
+            <div className="is-size-5">Accurancy:</div>
+            <p className="has-text-info is-size-1">{Math.round((correct / (correct + incorrect)) * 100)} %
+            </p>
           </div>
         </div>
       </div>
